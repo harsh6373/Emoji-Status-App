770335827640fc3a4a6a22f643d4fb6fdc9f844d
Harsh Jani <harshjani6373@gmail.com>
2022-07-10 15:09:10 +0530

    added Google signin in app

---
+++ app/src/main/java/com/example/emoji_status_app/SecondActivity.kt
@@ -0,0 +1,11 @@
+package com.example.emoji_status_app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SecondActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+    }
+}
